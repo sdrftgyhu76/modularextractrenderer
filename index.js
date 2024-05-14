22015f0fe039a95ea87899029e3342176f6fe61c
sdrftgyhu76 <gs1hfdsjkfh@hotmail.com>
2024-05-14 13:09:21 +0700

    test project

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
+const quickSortRandomPivot = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const pivotIndex = Math.floor(Math.random() * arr.length);
+  const pivot = arr[pivotIndex];
+  const left = [];
+  const right = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== pivotIndex) {
+      if (arr[i] <= pivot) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
+    }
+  }
+  return quickSortRandomPivot(left).concat(pivot, quickSortRandomPivot(right));
+};
