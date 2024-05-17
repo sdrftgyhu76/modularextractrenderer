4ab4289cb5012c258aa8cb44884615be51252b65
sdrftgyhu76 <gs1hfdsjkfh@hotmail.com>
2024-05-17 15:54:02 +0700

    start

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-function binarySearch(arr, target) {
-  let low = 0;
-  let high = arr.length - 1;
-  while (low <= high) {
-    let mid = Math.floor((low + high) / 2);
-    if (arr[mid] === target) return mid;
-    if (arr[mid] < target) low = mid + 1;
-    else high = mid - 1;
-  }
-  return -1;
-}
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(randomNumber);
